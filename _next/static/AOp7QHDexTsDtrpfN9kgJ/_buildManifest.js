4ae720f326e2744ec349d9cc3853c0eb9dbe540a
quanhieu <quanhieu@users.noreply.github.com>
2023-12-18 09:47:44 +0000

    deploy: c1f0fff0ef8f3dc317a8ac1d27f483a0a3f42c68

--- _next/static/AOp7QHDexTsDtrpfN9kgJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-ed2b369a14c2c0f2.js","static/chunks/833-d63f2d4e911d0b9f.js","static/chunks/pages/index-40d3f74d9c08b5cd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
